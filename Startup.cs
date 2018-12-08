1513e7c20546c4d14edb91b4341a3c17db358061
tstief <tstief@gmail.com>
2018-12-08 20:31:22 +0000

    Initial books api

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BooksApi.Services;
 
 namespace StiefWeb
 {
@@ -20,6 +21,7 @@ namespace StiefWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<BookService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
